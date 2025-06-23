3a7336cf3faf357c06c116d94f305e6a67f041dd
thanatos-vf-2000 <32266803+thanatos-vf-2000@users.noreply.github.com>
2025-06-24 00:01:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# esp32-c6-Linky-tic
+ESP32 C6 1.47 LCD - Linky TIC
---
+++ esp32-c6-Linky-tic.ino
@@ -0,0 +1,29 @@
+
+#include "src/Display_ST7789.h"
+#include "src/LVGL_Driver.h"
+#include "src/ui/ui.h"
+
+void setup()
+{       
+
+  LCD_Init();
+  Lvgl_Init();
+  ui_init();
+  
+}
+
+void loop()
+{
+  static uint32_t last_update =0;
+  if (millis() - last_update >= 2000) {
+    last_update = millis();
+
+    int cpt = random(0, 150);
+    lv_bar_set_value(objects.bar_test, cpt, LV_ANIM_OFF);
+    lv_label_set_text(objects.lbl_main, String(cpt).c_str());
+
+  }
+  
+  Timer_Loop();
+  delay(5);
+}
